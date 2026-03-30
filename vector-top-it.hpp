2f902d5f79e3e5acb64dde064745e28ebdd54e51
liia <kirilich_lubit@rambler.ru>
2026-03-30 13:29:04 +0300

    self assigment check

--- vector-top-it.hpp
+++ vector-top-it.hpp
@@ -14,14 +14,30 @@ namespace topit {
     Vector<T>& operator=(const Vector<T>&);
     Vector(Vector<T>&&) noexcept;
     Vector<T>& operator=(Vector<T>&&) noexcept;
-
-
     void swap(Vector<T>&) noexcept;
     bool isEmpty() const noexcept;
     size_t getSize() const noexcept;
     size_t getCapacity() const noexcept;
 
-    void push_back(const T& val);
+    void push_back(const T& val); // исправь название
+    void pushFront(const T& val);
+
+    //CW copy/swap добавляем изменения и своп + тестики
+    void insert(size_t pos, const T& val);
+    void insert(size_t pos, const Vector<T>& rhs, size_t b, size_t e);
+    void erase(size_t pos);
+
+    //DZ
+    //1. Руализовать итераторы для вектора, сами итераторы не тестировать
+    //2. придумать по три штуке insert/erase с итераторами 
+    //Например:
+    struct VectorIterator;
+    void insert(VectorIterator pos, const T& val);
+    void erase(VectorIterator pos);
+    template<class IT>
+    void insert(VectorIterator pos, IT begin, IT end);
+
+
 
     T& operator[](size_t id) noexcept;
     const T& operator[](size_t id) const noexcept;
@@ -40,6 +56,24 @@ namespace topit {
 }
 
 template < class T >
+void topit::Vector<T>::pushFront(const T& val)
+{
+  Vector<T> result(getSize() + 1);
+  result[0] = val;
+  for (size_t i = 0; i < getSize(); i++) {
+    result[i + 1] = (*this)[i];
+  }
+  swap(result);
+  /*
+  Vector<T> cpy(*this);
+  // поменять копию
+  // Если были исключения - деструктор копии освободит ресурсы
+  //                      - при этом сам вектор не изменится
+  // Если исключений не было то все отлично
+  swap(cpy);*/
+}
+
+template < class T >
 topit::Vector<T>::Vector(Vector<T>&& rhs) noexcept:
   data_(rhs.data_),
   size_(rhs.size_),
@@ -51,6 +85,11 @@ topit::Vector<T>::Vector(Vector<T>&& rhs) noexcept:
 template < class T >
 topit::Vector<T>& topit::Vector<T>::operator=(Vector<T>&& rhs) noexcept
 {
+  // тута лишняя проверка
+    /*if (this == std::addresof(rhs))
+    {
+      return *this;
+    }*/
   Vector<T> cpy(std::move(rhs)); //без move будет думтаь что lvalue
   swap(cpy);
   return *this;
@@ -69,6 +108,11 @@ topit::Vector<T>::Vector(const Vector<T>& rhs) :
 template < class T >
 topit::Vector<T>& topit::Vector<T>::operator=(const Vector<T>& rhs)
 {
+  // проверяй на присваивание самого себя? без проверки ссылочная стабильность у итераторов ломается. так как располоение в памяти меняется.
+    /*if (this == std::addresof(rhs))
+    {
+      return *this;
+    }*/
   Vector<T> cpy(rhs);
   swap(cpy);
   /*T* d = new T[rhs.getSize()];
